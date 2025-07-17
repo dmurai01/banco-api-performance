8be8954e138e01c04a71a9b563bc65b7245e04da
Daniela Murai <dany.murai@gmail.com>
2025-07-17 10:19:13 -0300

    adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,11 +3,9 @@ import { sleep, check } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '10s', target: 10 },
+        { duration: '5s', target: 10 },
         { duration: '20s', target: 10 },
-        { duration: '10s', target: 30 },
-        { duration: '20s', target: 30 },
-        { duration: '20s', target: 0 },
+        { duration: '5s', target: 0 }
     ],
     thresholds: {
         http_req_duration: ['p(90)<1000', 'max<5000'],
